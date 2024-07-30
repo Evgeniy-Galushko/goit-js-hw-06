00bf152c2183250b5eb5cc238557e9be9b6781c5
Galushko Evgeniy <yudginchik@gmail.com>
2024-07-30 17:48:58 +0300

    homework is ready

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,37 @@
 'use strict';
+class StringBuilder {
+  #value
+  constructor(value) {
+    this.#value = value;
+  }
+  getValue() {
+    return this.#value
+  }
+  padStart(str) {
+    const intermediateResult = this.#value = `${str + this.#value}` 
+    return intermediateResult
+  }
+  padEnd(str) {
+    const intermediateResult = this.#value = `${this.#value + str}` 
+    return intermediateResult;
+  }
+  padBoth(str) {
+    const result = this.#value = `${str + this.#value + str}` 
+    return result
+  }
+}
+
+
+
+
+const builder = new StringBuilder( "." );
+ console. log (builder.getValue()); //"." 
+builder.padStart( "^" );
+ console.log (builder.getValue()); // "^." 
+builder.padEnd( "^" );
+ console.log (builder.getValue()); // "^.^" 
+builder.padBoth( "=" );
+ console.log (builder.getValue()); // "=^.^="
+
 
 
